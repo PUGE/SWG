54ae133582e2b79f42833f935e2bdea20bdb0045
puge <mail@puge.org>
2019-03-06 11:20:38 +0800

    save

--- main.js
+++ main.js
@@ -10,10 +10,13 @@ let htmlTemple = `
     <title>Soulless Web Pages</title>
     <!-- *head* -->
     <style type="text/css">
-      .body {
+      html, body {
         width: 100%;
         height: 100%;
         margin: 0;
+      }
+      div:hover {
+        background-color: rgba(0, 0, 0, 0.20);
       }
     </style>
     <!-- css-output -->
@@ -30,6 +33,32 @@ psd.parse()
 
 
 const tree = psd.tree().descendants()
+
+function checkBG (info) {
+  if (info.left === 0 && info.top === 0 && info.right === info.width && info.bottom === info.height) {
+    return true
+  }
+  return false
+}
+
+const safetyZone = ''
+// 测试
+// tree.forEach(element => {
+//   const info = {
+//     file: `${element.name}.png`,
+//     left: element.left,
+//     right: element.right,
+//     top: element.top,
+//     bottom: element.bottom,
+//     height: element.height,
+//     width: element.width
+//   }
+//   // 判断背景，背景占据整个屏幕
+//   console.log(checkBG(info))
+  
+// })
+// return
+
 tree.forEach(element => {
   element.layer.image.saveAsPng(`./output/${element.name}.png`)
 })
@@ -47,8 +76,16 @@ tree.forEach(element => {
     height: element.height,
     width: element.width
   }
-  const style = `position: absolute; background: url(./${element.name}.png); width: ${info.width}px; height: ${info.height}px; left: ${info.left}px; top: ${info.top}px; z-index: ${tree.length - index};`
-  domHtml += `<div class="${element.name}" style="${style}"></div>\r\n    `
+  const styleList = [
+    'position: absolute',
+    `background-image: url(./${element.name}.png)`,
+    `width: ${info.width}px`,
+    `height: ${info.height}px`,
+    `left: ${info.left}px`,
+    `top: ${info.top}px`,
+    `z-index: ${tree.length - index}`
+  ]
+  domHtml += `<div class="${element.name}" style="${styleList.join('; ')};"></div>\r\n    `
   index++
 })
 htmlTemple = htmlTemple.replace(`<!-- page-output -->`, domHtml)
